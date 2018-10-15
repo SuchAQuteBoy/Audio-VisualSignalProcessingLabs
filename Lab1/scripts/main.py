3c19e5c57cda41de57edb10388c76f9a144ee702
SuchAQuteBoy <nyolala@outlook.com>
2018-10-15 21:07:26 +0800

    实验脚本

--- Lab1/scripts/main.py
+++ Lab1/scripts/main.py
@@ -3,41 +3,97 @@ import wave
 import numpy as np
 import matplotlib.pyplot as plt
 
+def sgn(a):
+    if a >= 0:
+        return 1
+    else:
+        return -1
 
-def ZeroCR(waveData, frameSize, overLap):
-    wlen = len(waveData)
-    step = frameSize - overLap
-    frameNum = math.ceil(wlen/step)
-    zcr = np.zeros((frameNum, 1))
+
+def ZeroCR(waveData, frameSize):
+    wave_len = len(waveData)
+    frameNum = math.ceil(wave_len / frameSize)
+    ZRresult = []
+    f = []
+    loop = 1
+    result = 0
     for i in range(frameNum):
-        curFrame = waveData[np.arange(i*step, min(i*step+frameSize, wlen))]
-        # To avoid DC bias, usually we need to perform mean subtraction on each frame
-        # ref: http://neural.cs.nthu.edu.tw/jang/books/audiosignalprocessing/basicFeatureZeroCrossingRate.asp
-        curFrame = curFrame - np.mean(curFrame)  # zero-justified
-        zcr[i] = sum(curFrame[0:-1]*curFrame[1::] <= 0)
-    return zcr
+        l = loop + frameSize
+        if l > wave_len:
+            l = wave_len
+        for j in range(loop, l):
+            result = result + (waveData[j]*waveData[j-1] < 0)
+        f.append(result)
+        result = 0
+        loop += frameSize
+    
+    for i in range(len(f)):
+        ZRresult.append(f[i] / (frameSize - 1))
+    return ZRresult
 
-fw = wave.open('Lab1/scripts/2.wav','rb')
-params = fw.getparams()
-print(params)
-nchannels, sampwidth, framerate, nframes = params[:4]
-str_data = fw.readframes(nframes)
-wave_data = np.fromstring(str_data, dtype=np.short)
-wave_data.shape = -1, 1
-#wave_data = wave_data.T
-fw.close()
-# calculate Zero Cross Rate
-frameSize = 256
-overLap = 0
-zcr = ZeroCR(wave_data,frameSize,overLap)
-# plot the wave
-time = np.arange(0, len(wave_data)) * (1.0 / framerate)
-time2 = np.arange(0, len(zcr)) * (len(wave_data)/len(zcr) / framerate)
-plt.subplot(211)
-plt.plot(time, wave_data)
-plt.ylabel("Amplitude")
-plt.subplot(212)
-plt.plot(time2, zcr)
-plt.ylabel("ZCR")
-plt.xlabel("time (seconds)")
-plt.show()
+
+def ZCR(inputName, extension):
+    fileName = inputName + extension
+    outputName = inputName + '_' + "zero.txt"
+    file = wave.open(fileName,"rb")
+    params = file.getparams()
+    nchannels, sampwidth, framerate, nframes = params[:4]
+    str_data = file.readframes(nframes)
+    file.close()
+
+    wave_data = np.fromstring(str_data, dtype=np.short)
+    # wave_data.shape = -1,1
+    frameSize = 256
+    result = ZeroCR(wave_data, frameSize)
+    file = open(outputName,"w+")
+    for i in range(len(result)):
+        file.write(str(result[i]) + '\n')
+    file.close
+
+
+def Energy(waveData, frameSize):
+    wave_len = len(waveData)
+    frameNum = math.ceil(wave_len / frameSize)
+    ENresult = []
+    loop = 0
+    result = 0
+    for i in range(frameNum):
+        l = loop + frameSize
+        if l > wave_len:
+            l = wave_len
+        for j in range(loop, l):
+            # if i == 162: print(str(j) + ' ' + str(waveData[j]) + ' ' + str(pow(waveData[j], 2)))
+            result = result + pow(waveData[j], 2) * pow(1.0 / (2 * frameSize), 2)
+        ENresult.append(result)
+        result = 0
+        loop += frameSize
+    # print(ENresult)
+    return ENresult
+
+def ENG(inputName, extension):
+    fileName = inputName + extension
+    outputName = inputName + '_' + "en.txt"
+    file = wave.open(fileName,"rb")
+    params = file.getparams()
+    nchannels, sampwidth, framerate, nframes = params[:4]
+    str_data = file.readframes(nframes)
+    file.close()
+
+    wave_data = np.fromstring(str_data, dtype=np.short)
+    # wave_data.shape = -1,1
+    frameSize = 256
+    result = Energy(wave_data, frameSize)
+    file = open(outputName,"w+")
+    for i in range(len(result)):
+        file.write(str(result[i]) + '\n')
+    file.close
+
+def main():
+    for i in range(1, 11):
+        name = str(i)
+        ZCR(name, ".wav")
+        ENG(name, ".wav")
+
+
+if __name__=="__main__":
+    main()

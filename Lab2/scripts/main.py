79ce265deae0d4d477a1d6dd95f278a25c40651b
SuchAQuteBoy <nyolala@outlook.com>
2018-11-12 15:24:33 +0800

    主函数

--- Lab2/scripts/main.py
+++ Lab2/scripts/main.py
@@ -1,15 +1,40 @@
 # -*- coding: utf-8 -*-
 
-from matplotlib import pyplot as plt 
-from ReadWAVFile import rwf
+from matplotlib import pyplot as plt
+from ReadWAVFile import rff, wtf
+from dpcl import Dpcl
 
-def read_file(filename):
-    file = rwf(filename)
-    raw_data = file.getRawData()
+class Main(object):
 
+    def _read_file(self, filename):
+        file = rff(filename)
+        data = file.getFrameListData()
+        data, rebuild, err, snr = Dpcl(data).return_data()
+        self._data = data
+        self._rebuild = rebuild
+        self._err = err
+        self._snr = snr
 
-def main():
-    print("Hello World!")
+    def _write_err_file(self, filename):
+        wtf(filename, self._err)
+
+    def _write_rebuild_file(self, filename):
+        wtf(filename, self._rebuild)
+    
+    def _print_snr(self, filename):
+        string = "SNR for {0} : {1}".format(filename, self._snr)
+        print(string)
+
+    def main(self):
+        for i in range(1, 11):
+            filename_wav = "../resources/" + str(i) + ".wav"
+            filename_err = "../out/" + str(i) + ".dpc"
+            filename_rebuild = "../out/" + str(i) + ".pcm"
+            self._read_file(filename_wav)
+            self._write_err_file(filename_err)
+            self._write_rebuild_file(filename_rebuild)
+            self._print_snr(filename_wav)
+
 
 if __name__ == '__main__':
-    main()
+    Main().main()
